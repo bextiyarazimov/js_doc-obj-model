00c02fa3d37850d03022a84ccb4870c5b35eb068
Baxtiyar Azimov <baxtiyar.azimov@aahm.lan>
2023-11-23 12:17:39 +0400

    add

---
+++ main.js
@@ -0,0 +1,61 @@
+
+
+///// Task 1: Change the Background Color
+// Create an HTML file with a button. When the button is clicked, change the background color of the page to a random color.
+/// Tapşırıq 1: Fon rəngini dəyişdirin
+// Düymə ilə HTML faylı yaradın. Düymə kliklədikdə, səhifənin fon rəngini təsadüfi rəngə dəyişdirin.
+
+
+// function changeBackgroundColor() {
+//     // Generate a random color
+//     const randomColor = getRandomColor();
+
+//     // Change the background color
+//     document.body.style.backgroundColor = randomColor;
+// }
+
+// function getRandomColor() {
+//     // Generate a random hexadecimal color code
+//     return '#' + Math.floor(Math.random()*16777215).toString(16);
+// }
+
+
+
+
+
+
+
+
+///Task 2: Create an HTML file with a button and a counter display. When the button is clicked, increment the counter and update the display.
+// Tapşırıq 2: Düymə və əks ekranla HTML faylı yaradın. Düymə kliklədikdə sayğacı artırın və ekranı yeniləyin.
+
+
+
+
+// let counter = 0;
+
+// function incrementCounter() {
+//     counter++;
+//     document.getElementById("counterDisplay").innerText = "Counter: " + counter;
+// }
+
+
+
+
+
+
+
+
+///Task 3: Create an HTML element for the image tag and show the screen an image then create a button in HTML and write a function in JS for button click. When click the button change the image source(src) attribute
+// Tapşırıq 3: Şəkil etiketi üçün HTML elementi yaradın və ekrana şəkil göstərin, sonra HTML-də bir düymə yaradın və düyməni basmaq üçün JS-də funksiya yazın. Düyməni kliklədikdə şəkil mənbəyi(src) atributunu dəyişdirin
+
+
+// let imageElement = document.getElementById("myImage");
+
+// function changeImage() {
+//     // Change the image source (src) attribute
+//     imageElement.src = "new-image.jpg";
+//     // You can replace "new-image.jpg" with the path to your new image.
+// }
+
+
